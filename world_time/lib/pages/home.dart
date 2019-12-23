b90a7a6b9494c5bb4f6674c7a8802c7aebdc7368
Lagrango <davidnlagrange@hotmail.com>
2019-12-23 12:04:01 -0600

    Clock Functionality + Dynamic Background + API Call

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -6,20 +6,75 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+
+    data = ModalRoute.of(context).settings.arguments;
+    print(data);
+    // set background
+    String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDayTime'] ? Colors.blue : Colors.indigo;
+    Color textColor = data['isDayTime'] ? Colors.grey[800] : Colors.white;
+    Color buttonColor = data['isDayTime'] ? Colors.grey[900] : Colors.grey[300];
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/$bgImage'),
+                fit: BoxFit.cover,
               ),
-            ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+              child: Column(
+                children: <Widget>[
+                  FlatButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: buttonColor,
+                    ),
+                    label: Text(
+                      'Edit Location',
+                      style: TextStyle(
+                        color: buttonColor,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        data['location'],
+                      style: TextStyle(
+                        fontSize: 28,
+                        letterSpacing: 2,
+                        color: textColor,
+                      ),
+                      ),
+
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    data['time'],
+                    style: TextStyle(
+                      fontSize: 66,
+                      color: textColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           )
       ),
     );
